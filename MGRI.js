27fc6814a59bcae482796434efd1712f4c36e78a
prospectCustomization <75590157+prospectCustomization@users.noreply.github.com>
2020-12-07 01:21:03 -0500

    Add files via upload

--- MGRI.js
+++ MGRI.js
@@ -2,47 +2,83 @@
 var swingweight;
 var balance;
 var massGrams;
+var massKg;
 var mgri;
+const gravity = 980.5;
 
-function calculateMGRI(){
+function specsForm(){
 
+	// Had to include parseFloat; swingweight was for some reason going as string.
 	swingweight = parseFloat(document.getElementById("swingweight").value);
 	balance = parseFloat(document.getElementById("balance").value);
 	massGrams = parseFloat(document.getElementById("mass").value);
-	// Had to include parseInt; swingweight was for some reason going as string.
-	var massKg = massGrams * .001;
-	const gravity = 980.5;
+	
+	massKg = massGrams * .001;
 
 	// 10510 / (325 + (20 * (.335) * 32) - (100 * .335)) = 20.77
 
-	inertia = swingweight + (20 * massKg * balance) - 100 * massKg;
+	var inertia = swingweight + (20 * massKg * balance) - 100 * massKg;
 	mgri = (massKg * gravity * balance) / inertia;
-	var mgriBackhand = massKg * gravity * (balance - 10) / swingweight
+	var mgriBackhand = massKg * gravity * (balance - 10) / swingweight;
 
 	//var 
 
 	if(mgri == 0 || mgri === null || isNaN(mgri)){
 		console.log("Missing info");
-		document.getElementById("mgriDesired").disabled = true;
-		document.getElementById("mgriDesired").type = "text";
-		document.getElementById("mgriDesired").value = "Enter specs first.";
+		document.getElementById("desiredMgri").disabled = true;
+		document.getElementById("desiredMgri").type = "text";
+		document.getElementById("desiredMgri").value = "Enter specs first.";
 	}
 	else {
 		document.getElementById("mgri").value = mgri.toFixed(3);
 		document.getElementById("mgriBackhand").value = mgriBackhand.toFixed(3);
-		document.getElementById("mgriDesired").disabled = false;
-		document.getElementById("mgriDesired").type = "number";
+		document.getElementById("desiredMgri").disabled = false;
+		document.getElementById("desiredMgri").type = "number";
 		calculateSpecs();
 	}
 }
 
 function calculateSpecs(){
-	var mgriDesired = document.getElementById("mgriDesired");
-	console.log(swingweight);
-	while(mgriDesired > mgri){
-		console.log("Nope");
+	var desiredMgri = parseFloat(document.getElementById("desiredMgri").value);
+
+	var adjustedSwingweight = swingweight;
+	var adjustedBalance = balance;
+	var adjustedMassKg = massKg;
+	var inertia;
+	var currentMgri = mgri;
+
+	// If MGRI is higher than desired, raise SW until corrected.
+
+	if(currentMgri > desiredMgri && (desiredMgri > 18 && desiredMgri < 23)){
+		while(currentMgri > desiredMgri && adjustedSwingweight < 600){
+			adjustedSwingweight = adjustedSwingweight + 1;
+			adjustedBalance = adjustedBalance + (0.1 / 3);
+			adjustedMassKg = adjustedMassKg + (.001 / 3);
+
+			var inertia = adjustedSwingweight + (20 * adjustedMassKg * adjustedBalance) - 100 * adjustedMassKg;
+			currentMgri = (adjustedMassKg * gravity * adjustedBalance) / inertia;
+		}
 	}
-	//	console.log(swingweight);
-	//	swingweight = swingweight + 1;
-	//}
+	else if (currentMgri < desiredMgri && (desiredMgri > 18 && desiredMgri < 23)){
+		while(currentMgri < desiredMgri && adjustedSwingweight < 600){
+			// May want to adjust these values; particularly sw.
+			adjustedSwingweight = adjustedSwingweight + 0.1;
+			adjustedBalance = adjustedBalance - (0.1 / 3);
+			adjustedMassKg = adjustedMassKg + .001;
+
+			var inertia = adjustedSwingweight + (20 * adjustedMassKg * adjustedBalance) - 100 * adjustedMassKg;
+			currentMgri = (adjustedMassKg * gravity * adjustedBalance) / inertia;
+		}
+	}
+
+	else {
+		document.getElementById("adjustedSwingweight").value = "";
+		document.getElementById("adjustedBalance").value = "";
+		document.getElementById("adjustedMass").value = "";
+		return;
+	}
+	
+	document.getElementById("adjustedSwingweight").value = Math.round(adjustedSwingweight);
+	document.getElementById("adjustedBalance").value = Math.round(adjustedBalance * 10) / 10;
+	document.getElementById("adjustedMass").value = Math.round(adjustedMassKg * 1000);
 }
